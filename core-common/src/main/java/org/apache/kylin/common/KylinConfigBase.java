c6c8051f56abffda191aaa1fc3328fcdaa9bad75
shaofengshi <shaofengshi@apache.org>
2018-05-20 21:57:37 +0800

    set kylin.query.enable-dynamic-column to true by default

--- core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -1230,7 +1230,7 @@ abstract public class KylinConfigBase implements Serializable {
     // check KYLIN-3358, need deploy coprocessor if enabled
     // finally should be deprecated
     public boolean isDynamicColumnEnabled() {
-        return Boolean.valueOf(getOptional("kylin.query.enable-dynamic-column", "false"));
+        return Boolean.valueOf(getOptional("kylin.query.enable-dynamic-column", "true"));
     }
 
     //check KYLIN-1684, in most cases keep the default value
